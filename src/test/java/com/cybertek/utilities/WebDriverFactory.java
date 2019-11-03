fc929d6ed8cf61dae09ce8d3806d3f919e4138e1
javamann007 <javamann007@gmail.com>
2019-11-03 10:35:40 +0100

    First Commit

---
+++ src/test/java/com/cybertek/utilities/WebDriverFactory.java
@@ -0,0 +1,32 @@
+package com.cybertek.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+
+    // factory class returns certain type of objects based on inputs
+
+    public static WebDriver getDriver (String browserType) {
+
+        WebDriver driver = null;
+
+        switch (browserType.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+        }
+        return driver;
+
+
+
+    }
+
+
+}
